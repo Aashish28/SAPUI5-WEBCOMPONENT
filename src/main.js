207a5dc90d2a77df12f54f3a4e9253000ef3c66e
Ahire <ashish.ahire@accenture.com>
2019-07-01 16:22:26 +0530

    Parcel based server and ui5 web component

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import "@ui5/webcomponents/dist/Button";
+import "@ui5/webcomponents/dist/TextArea";
+import "@ui5/webcomponents/dist/Input";
+import "@ui5/webcomponents/dist/DatePicker";
+import "@ui5/webcomponents/dist/Icon";
+import "@ui5/webcomponents/dist/Popover";
+import "@ui5/webcomponents/dist/TabContainer"; 
+import "@ui5/webcomponents/dist/Tab"; 
+import "@ui5/webcomponents/dist/TabSeparator";
+import "@ui5/webcomponents/dist/Badge";
+import "@ui5/webcomponents/dist/ShellBar";
